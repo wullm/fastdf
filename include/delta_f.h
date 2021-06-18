da30d5ba1003ee488fd85163305b1e88da92786d
Willem Elbers <willem.h.elbers@durham.ac.uk>
2021-06-18 07:01:41 +0100

    Fix undefined behaviour

--- include/delta_f.h
+++ include/delta_f.h
@@ -44,6 +44,7 @@ static inline uint64_t splitmix64(uint64_t *state) {
     uint64_t result;
 
     *state = *state + 0x9E3779B97f4A7C15;
+    result = *state;
     result = (result ^ (result >> 30)) * 0xBF58476D1CE4E5B9;
     result = (result ^ (result >> 27)) * 0x94D049BB133111EB;
     return result ^ (result >> 31);
