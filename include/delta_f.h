7eab64d09af3680e5468d59418df71446df5b451
Willem Elbers <willem.h.elbers@durham.ac.uk>
2021-06-18 06:57:42 +0100

    Change order of RNG to match other implementation

--- include/delta_f.h
+++ include/delta_f.h
@@ -41,9 +41,9 @@ static inline double hypot3(double x, double y, double z) {
 
 /* Pseudo-random number generator */
 static inline uint64_t splitmix64(uint64_t *state) {
-    uint64_t result = *state;
+    uint64_t result;
 
-    *state = result + 0x9E3779B97f4A7C15;
+    *state = *state + 0x9E3779B97f4A7C15;
     result = (result ^ (result >> 30)) * 0xBF58476D1CE4E5B9;
     result = (result ^ (result >> 27)) * 0x94D049BB133111EB;
     return result ^ (result >> 31);
