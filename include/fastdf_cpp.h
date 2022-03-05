e5b2911fdd049c9da8a4d8e3ebd23f1670d80f85
Willem Elbers <whe@willemelbers.com>
2022-03-05 03:59:32 +0000

    Also compile a library

---
+++ include/fastdf_cpp.h
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * This file is part of Mitos.
+ * Copyright (c) 2020 Willem Elbers (whe@willemelbers.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+
+#ifndef FASTDF_CPP_H
+#define FASTDF_CPP_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "input.h"
+#include "output.h"
+#include "message.h"
+#include "delta_f.h"
+#include "header.h"
+#include "perturb_data.h"
+#include "perturb_spline.h"
+#include "particle.h"
+#include "titles.h"
+#include "cosmology.h"
+#include "fft.h"
+#include "fft_kernels.h"
+#include "relativity.h"
+#include "mesh_grav.h"
+
+/* Did we compile with CLASS? */
+#ifdef WITH_CLASS
+#include "classex.h"
+#endif
+
+int run_fastdf();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
